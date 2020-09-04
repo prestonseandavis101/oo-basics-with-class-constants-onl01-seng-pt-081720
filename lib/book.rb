bf883f2812abfcf14b802f2b07f776420ac5030d
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-04 03:12:55 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
-class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre
+class Book #<-- defines our class
+  attr_accessor :author, :page_count # <--- the setter and getter methods are the same
+  attr_reader :title, :genre # <--- allows these titles to be read
   
-  GENRES = [] #<---constant 
+  GENRES = [] # <---constant variable, availainle to all instances of book class 
 
   def initialize(title)
     @title = title
